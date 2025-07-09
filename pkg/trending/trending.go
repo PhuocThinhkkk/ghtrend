0d22caee879e6f86ee373c7b4935d65c89580279
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-07-09 10:05:26 +0000

    i am so back :rotating_light:

--- pkg/trending/trending.go
+++ pkg/trending/trending.go
@@ -5,36 +5,39 @@ import (
 	"fmt"
 	"strings"
 	"github.com/PuerkitoBio/goquery"
+	"ghtrend/pkg/types"
 )
 
-type Repo struct {
-    Index       int    `json:"index"`
-    Name        string `json:"name"`
-    Url         string `json:"url"`
-    Description string `json:"description"`
-    Language    string `json:"language"`
-    Stars       string `json:"stars"`
-    Forks       string `json:"forks"`
-}
 
-func ParseHtml(html string) ([]Repo, error) {
+func ParseHtml(html string) ([]types.Repo, error) {
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
 	if err != nil {
 		fmt.Println("error when parsing html")
 		return nil, err
 	}
-	var repos    []Repo
+	var repos    []types.Repo
 	doc.Find("article.Box-row").Each(func(i int, s *goquery.Selection) {
 		name := strings.TrimSpace(s.Find("h2 a").Text())
+		owner, repoName := "", ""
+		if parts := strings.Split(name, "/\n\n      "); len(parts) == 2 {
+			repoName = parts[1]
+			for j := range len(parts[0]){
+				if j == 0 {
+					continue
+				}
+				owner += string(parts[0][j])
+			}
+		}
 		url, _ := s.Find("h2 a").Attr("href")
 		description := strings.TrimSpace(s.Find("p").Text())
 		lang := strings.TrimSpace(s.Find("span[itemprop='programmingLanguage']").Text())
 		stars := strings.TrimSpace(s.Find("a[href$='/stargazers']").First().Text())
 		forks := strings.TrimSpace(s.Find("a[href$='/network/members']").First().Text())
 
-		repos = append(repos, Repo{
+		repos = append(repos, types.Repo{
 			Index:       i,
-			Name:        name,
+			Owner:       owner,
+			Name:        repoName,
 			Url:         "https://github.com" + url,
 			Description: description,
 			Language:    lang,
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -2,13 +2,20 @@ package ui
 
 import (
     tea "github.com/charmbracelet/bubbletea"
-    "fmt"
-    "ghtrend/pkg/utils"
+	"github.com/charmbracelet/lipgloss"
+	"ghtrend/pkg/types"
+	"github.com/charmbracelet/bubbles/table"
 )
 
+var debugMode = true
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+
 type Model struct {
-    Repos  []utils.Repo
-    cursor int
+   	table table.Model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -16,52 +23,67 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
     switch msg := msg.(type) {
     case tea.KeyMsg:
         switch msg.String() {
         case "q", "ctrl+c":
             return m, tea.Quit
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
-        case "down", "j":
-            if m.cursor < len(m.Repos)-1 {
-                m.cursor++
-            }
+      
         }
     }
-    return m, nil
+    m.table, cmd = m.table.Update(msg)		
+	return m, cmd
 }
-
 func (m Model) View() string {
-    s := "Trending github repos: \n\n"
-    for i, repo := range m.Repos {
-        cursor := " "
-        if m.cursor == i {
-            cursor = ">"
-        }
-        s += fmt.Sprintf(" %s %s %s - %s\n",
-            cursor,
-            repo.Name,
-            repo.Stars,
-            repo.Language,
-        )
-    }
+	return baseStyle.Render(m.table.View())
 
-    if len(m.Repos) > 0 && m.cursor < len(m.Repos) {
-        s += fmt.Sprintf("\n    %s\n", m.Repos[m.cursor].Description)
-    }
-
-    s += "\n[Press ↑/↓ to navigate, q to quit]"
-    return s
 }
 
-func Render(repos []utils.Repo) (tea.Model, error) {
-    m := Model{
-        Repos:  repos,
-        cursor: 0,
-    }
+func Render(repos []types.Repo) (tea.Model, error) {
+   	columns := []table.Column{
+		{Title: "Owner", Width: 15 },
+		{Title: "Repo name", Width: 25},
+		{Title: "Stars ", Width: 10},
+		{Title: "Forks", Width: 10},
+		{Title: "Language", Width: 15},
+	}
+	var rows []table.Row
+
+	for _, repo := range repos {
+		rows = append(rows, table.Row{
+			repo.Owner,
+			repo.Name,
+			repo.Stars,
+			repo.Forks,
+			repo.Language,
+		})
+	}
+
+
+		t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(15),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("60")).     // muted purple border
+	Foreground(lipgloss.Color("183")).          // soft lavender text
+	Background(lipgloss.Color("54")).           // dark purple background
+	BorderBottom(true).
+	Bold(true)
+
+	s.Selected = s.Selected.
+	Foreground(lipgloss.Color("16")).           // near-black text
+	Background(lipgloss.Color("99")).           // hot pink-purple highlight
+	Bold(true)
+	t.SetStyles(s)
+
+	m := Model{t}
     p := tea.NewProgram(m)
     return p.Run()
 }
